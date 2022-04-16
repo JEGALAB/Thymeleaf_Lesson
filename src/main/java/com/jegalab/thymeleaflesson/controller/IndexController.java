acd691a84bcf79e585b5b9bb252220724c7add50
JEGALAB <jegalab@gmail.com>
2022-04-16 14:07:06 -0500

    Loop in index with thymeleaf, rendering a list

--- src/main/java/com/jegalab/thymeleaflesson/controller/IndexController.java
+++ src/main/java/com/jegalab/thymeleaflesson/controller/IndexController.java
@@ -1,12 +1,25 @@
 package com.jegalab.thymeleaflesson.controller;
 
+import com.jegalab.thymeleaflesson.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService){
+        this.productService = productService;
+    }
+
     @RequestMapping("/")
-    public String getIndex(){
+    public String getIndex(Model model){
+        model.addAttribute("products", productService.listProducts());
+
         return "index";
     }
 }
